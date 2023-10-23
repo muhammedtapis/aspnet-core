c1fc036f4d9f31b519b40df636c335156ba9510b
muhammedtapis <muhammedtapis@gmail.com>
2023-10-23 18:17:20 +0300

    try-catch-finally ,throwing customer esceptions,global error handling,middleware

--- my-books/Controllers/PublishersController.cs
+++ my-books/Controllers/PublishersController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using my_books.Data.Services;
 using my_books.Data.ViewModels;
+using my_books.Exceptions;
+using System;
 
 namespace my_books.Controllers
 {
@@ -24,8 +26,22 @@ namespace my_books.Controllers
         [HttpPost("add-publisher")]
         public IActionResult AddPublisher([FromBody] PublisherVM publisher) //FromBody html bodyden gelen publisher name entrysi için.
         {
-            _publishersService.AddPublisher(publisher);
-            return Ok();
+            try
+            {
+                var newPublisher = _publishersService.AddPublisher(publisher);
+                return Created(nameof(AddPublisher), newPublisher); // created methodu oluşturulan objeyi ve method ismini alıyor.
+                                                                    // return ok yerine return created yapmamızın sebebi yeni bir resource oluştuğu için.
+            }
+            catch (PublisherNameException ex) //yeni oluşturduğumuz exception classını çağırdık.
+            {
+                return BadRequest($"{ex.Message}, Publisher name :{ex.PublisherName}"); //bu parametreler publishernameexception sınıfındaki constructorlardan en alttakinden geliyor.
+                //servisteki exception methodunun ilk parametresi  , methodun ikinci parametresi   !!!publisher serviste throw edildi bu hata burada yakalayıp kullanıcıya gösteriyoruz.
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+           
 
         }
 
@@ -37,13 +53,41 @@ namespace my_books.Controllers
             return  Ok(_response);
         }
 
+
+        [HttpGet("get-publisher-by-id/{id}")]
+
+        public IActionResult GetPublisherById(int id)
+        {
+           
+            var _response = _publishersService.GetPublisherById(id);
+
+            if(_response != null)
+            {
+                return Ok(_response);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+        }
+
         [HttpDelete("delete-publisher-by-id/{id}")]
 
         public IActionResult DeletePublisherById(int id) 
         {
-            _publishersService.DeletePublisherById(id);
+            try
+            {
+                _publishersService.DeletePublisherById(id);
 
-            return Ok();
+                return Ok();
+            }
+            catch (System.Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+
         }
     }
 }
---
+++ my-books/Exceptions/PublisherNameException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace my_books.Exceptions
+{
+    public class PublisherNameException :Exception  //exception classı oluşturabilmen için parenttan inherit alman gerek.
+    {
+        public string PublisherName { get; set; }
+
+        public PublisherNameException()
+        {
+                
+        }
+
+        public PublisherNameException(string message):base(message)
+        {
+                
+        }
+
+        public PublisherNameException(string message,Exception inner):base(message, inner) 
+        {
+                
+        }
+
+        public PublisherNameException(string message ,string publisherName):this(message)
+        {
+            PublisherName = publisherName;  
+        }
+    }
+}
