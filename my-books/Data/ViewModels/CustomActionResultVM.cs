b8d4e43653b2da9c2cc915cf684ed318310347ab
muhammedtapis <muhammedtapis@gmail.com>
2023-10-26 17:42:09 +0300

    action return types

---
+++ my-books/Data/ViewModels/CustomActionResultVM.cs
@@ -0,0 +1,15 @@
+﻿using my_books.Data.Models;
+using System;
+
+namespace my_books.Data.ViewModels
+{
+    public class CustomActionResultVM
+    {
+
+        public Exception Exception { get; set; }
+      //  public object Data { get; set; } //genel olarak dönmek istediğin data olunca böyle 
+
+        public Publisher Publisher {  get; set; } //Spesifik return type.
+    }
+}
+
